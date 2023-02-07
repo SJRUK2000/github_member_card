df4758af1dfa259ac3ef9665f78e1572031773bb
SJRUK2000 <you@example.com>
2023-02-07 16:13:11 -0500

    comments added, error handling

--- src/components/MemberCard.jsx
+++ src/components/MemberCard.jsx
@@ -11,6 +11,7 @@ const MemberCardWrapper = styled.div`
   border-radius: 10px;
   margin: 20px;
 
+  /* Width reduced for smaller screens */
   @media (max-width: 600px) {
     width: 90%;
   }
@@ -21,37 +22,68 @@ const Avatar = styled.img`
   height: 100px;
   border-radius: 50px;
 
+/* Avatar reduced for smaller screens */
   @media (max-width: 600px) {
     width: 50px;
     height: 50px;
   }
 `;
+
 const MemberCard = ({ member }) => {
-    const [user, setUser] = useState({});
-  
-    useEffect(() => {
-      const fetchUser = async () => {
-        const response = await fetch(`https://api.github.com/users/${member.login}`, { 
+  /* state object to hold user information */
+  const [user, setUser] = useState({});
+  /* state to hold error message */
+  const [error, setError] = useState(null);
+
+  /* 
+    make an API call to retrieve 
+    information about the user using their `login` 
+    as a path parameter in the API URL. 
+  */
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${member.login}`, {
           headers: {
             Authorization: 'Bearer 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'
+          }
+        });
+        if (!response.ok) {
+          throw new Error(`Request failed with status code: ${response.status}`);
         }
-      });
+        /* Store the API response data in the user state */
         const data = await response.json();
         setUser(data);
-      };
-      fetchUser();
-    }, [member]);
-  
-    return (
-      <MemberCardWrapper>
-        <a href={user.html_url}>{user.login}</a>
-        <Avatar src={user.avatar_url} alt={`Avatar for ${user.login}`} />
-        {user.name && <p>Name: {user.name}</p>}
-        {user.location && <p>Location: {user.location}</p>}
-        {user.email && <p>Email: {user.email}</p>}
-        <p>Number of Public Repositories: {user.public_repos}</p>
-      </MemberCardWrapper>
-    );
-  };
-  
-  export default MemberCard;
+      } catch (error) {
+        /* Store the error message in the error state */
+        setError(error.message);
+      }
+    };
+    fetchUser();
+    /* fetch user data from API when component is first rendered and when `member` prop changes */
+  }, [member]);
+
+  return (
+    <MemberCardWrapper>
+      {/* If there was an error, show the error message */}
+      {error ? (
+        <p>An error occurred: {error}</p>
+      ) : (
+        <>
+          {/* Show the user login and a link to their Github profile */}
+          <a href={user.html_url}>{user.login}</a>
+          <Avatar src={user.avatar_url} alt={`Avatar for ${user.login}`} />
+          {/* Show the user's name, location and email if it's available */}
+          {user.name && <p>Name: {user.name}</p>}
+          {user.location && <p>Location: {user.location}</p>}
+          {user.email && <p>Email: {user.email}</p>}
+          {/* Show the number of public repositories the user has */}
+          <p>Number of Public Repositories: {user.public_repos}</p>
+        </>
+      )}
+    </MemberCardWrapper>
+  );
+};
+
+export default MemberCard;
--- src/components/MemberDirectory.jsx
+++ src/components/MemberDirectory.jsx
@@ -11,30 +11,47 @@ const MemberDirectoryWrapper = styled.div`
 `;
 
 const MemberDirectory = () => {
+  /*An array to store the members data obtained from the API call.*/
   const [members, setMembers] = useState([]);
+  /* The API uses a zero based index for pages and users */
   const [currentPage, setCurrentPage] = useState(0);
+  /* state to hold error message */
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchMembers = async () => {
-      const response = await fetch(
-        `https://api.github.com/users?per_page=10&since=${currentPage}`
-      );
-      const data = await response.json();
-      setMembers(data);
+      try {
+        const response = await fetch(
+          `https://api.github.com/users?per_page=10&since=${currentPage}`
+        );
+        if (!response.ok) {
+          throw new Error(`Failed to fetch members with status code: ${response.status}`);
+        }
+        const data = await response.json();
+        /* Store the API response data in the member state */
+        setMembers(data);
+        setError(null);
+      } catch (err) {
+        /* Store the error message in the error state */
+        setError(err.message);
+      }
     };
     fetchMembers();
+    /* fetch users from API when component is first rendered and when `currentPage` prop changes */
   }, [currentPage]);
 
   const goToNextPage = () => {
     setCurrentPage(currentPage + 19);
   };
-
+/*the API uses the since parameter to filter the results to return users whose ID is greater than or equal to a specific value.
+ incrementing the currentPage value by 19 moves to the next set of ten users whose ID is greater than the currentPage value.*/
   const goToPrevPage = () => {
     setCurrentPage(currentPage - 19);
   };
 
   return (
     <>
+      {error && <p>An error occurred: {error}</p>}
       <MemberDirectoryWrapper>
         {members.map((member) => (
           <MemberCard key={member.id} member={member} />
